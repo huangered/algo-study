e55fd7e32bfc463cd9cf3c2a7f59eed20937784f
huang yi <huangered@hotmail.com>
2016-10-16 19:50:59 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by huangered on 2016/10/16.
+//  Copyright © 2016年 huangyi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
